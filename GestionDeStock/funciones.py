94015b0e0addfdc26013cc4f7a523ce4a535d534
SantyPrado01 <santiago01prado@gmail.com>
2023-09-03 18:54:01 -0300

    Cambio en la busqueda

--- GestionDeStock/funciones.py
+++ GestionDeStock/funciones.py
@@ -110,12 +110,12 @@ def agregar_producto(ventana):
     cursor = base_datos.cursor()
 
     def insertar_marca(marca):
-        # Inserta la marca en la tabla 'marca' si no existe
+        
         cursor.execute("INSERT OR IGNORE INTO marca (nombre) VALUES (?)", (marca,))
         base_datos.commit()
 
     def insertar_categoria(categoria):
-        # Inserta la categoría en la tabla 'categoria' si no existe
+        
         cursor.execute("INSERT OR IGNORE INTO categoria (nombre) VALUES (?)", (categoria,))
         base_datos.commit()
 
@@ -126,7 +126,7 @@ def agregar_producto(ventana):
         marca = marca_seleccionada.get()
         categoria = categoria_seleccionada.get()
     
-        # Inserta la marca y la categoría si no existen
+        
         insertar_marca(marca)
         insertar_categoria(categoria)
 
@@ -136,11 +136,11 @@ def agregar_producto(ventana):
         cursor.execute("SELECT categoria_id FROM categoria WHERE nombre = ?", (categoria,))
         categoria_id = cursor.fetchone()
 
-        # Inserta el producto en la tabla 'producto'
+       
         cursor.execute("INSERT INTO producto (nombre, precio, stock, marca_id, categoria_id) VALUES (?, ?, ?, ?, ?)",
                    (nombre, precio, stock, marca_id[0], categoria_id[0]))
 
-        # Guarda los cambios en la base de datos
+        
         base_datos.commit()
         messagebox.showinfo('Completado','El producto ha sido guardado con éxito.')
 
@@ -190,9 +190,16 @@ def buscar_producto(ventana):
 
         else:
             # Muestra los resultados 
+
             fila = 1
             for resultado in productos:
                 id, nombre, precio, stock, marca_id, categoria_id = resultado
+
+                cursor.execute("SELECT nombre FROM marca WHERE marca_id=?", (marca_id,))
+                marca_nombre = cursor.fetchone()[0]
+
+                cursor.execute("SELECT nombre FROM categoria WHERE categoria_id=?", (categoria_id,))
+                categoria_nombre = cursor.fetchone()[0]
 
                 resultado_nombre = Label(ventana_emergente, text=f'{nombre}', font=45)
                 resultado_nombre.grid(row=fila, column=2)
@@ -203,13 +210,13 @@ def buscar_producto(ventana):
                 resultado_stock = Label(ventana_emergente, text=f'{stock}', font=45)
                 resultado_stock.grid(row=fila, column=4)
 
-                resultado_marca = Label(ventana_emergente, text=f'{marca_id}', font=45)
+                resultado_marca = Label(ventana_emergente, text=f'{marca_nombre}', font=45)
                 resultado_marca.grid(row=fila, column=5)
 
-                resultado_categoria = Label(ventana_emergente, text=f'{categoria_id}', font=45)
+                resultado_categoria = Label(ventana_emergente, text=f'{categoria_nombre}', font=45)
                 resultado_categoria.grid(row=fila, column=6)
 
-                boton_editar = Button(ventana_emergente, text='Editar Producto', command=lambda:mostrar_formulario_edicion(resultado), font=45)
+                boton_editar = Button(ventana_emergente, text='Editar Producto', command=lambda:mostrar_formulario_edicion(resultado, marca_nombre, categoria_nombre), font=45)
                 boton_editar.grid(row=fila, column=7, padx=5, pady=5)
 
                 boton_eliminar = Button(ventana_emergente, text='Eliminar Producto', command=lambda:eliminar_productos(id), font=45)
@@ -217,7 +224,6 @@ def buscar_producto(ventana):
 
                 fila += 1
 
-        # Cierra la conexión a la base de datos cuando hayas terminado
         base_datos.close()
 
     boton_buscar = Button(ventana_emergente, text='Buscar Producto', command=producto_buscar, font=45)
@@ -236,14 +242,14 @@ def eliminar_productos(id):
     messagebox.showinfo('Completado','El producto ha sido eliminado con éxito.')
 
 
-def mostrar_formulario_edicion(producto):
+def mostrar_formulario_edicion(producto, marca_seleccionada, categoria_seleccionada):
 
     ventana_emergente = Toplevel()
 
     ventana_emergente.resizable(width=False, height=False)
 
     ventana_emergente.title('Editar Producto')
-
+    
     titulo = Label(ventana_emergente, text='Editar Producto', font=65)
     titulo.grid(row=0, columnspan=2, padx=10, pady=10)
 
@@ -252,21 +258,21 @@ def mostrar_formulario_edicion(producto):
 
     nombre_producto = Entry(ventana_emergente, font=45)
     nombre_producto.grid(row=1, column=1)
-    nombre_producto.insert(0, producto[1])  # Llena el campo con el nombre del producto
+    nombre_producto.insert(0, producto[1])  
 
     titulo_precio = Label(ventana_emergente, text='Precio', font=45)
     titulo_precio.grid(row=2, column=0, padx=5, pady=5)
 
     precio_producto = Entry(ventana_emergente, font=45)
     precio_producto.grid(row=2, column=1)
-    precio_producto.insert(0, producto[2])  # Llena el campo con el precio del producto
+    precio_producto.insert(0, producto[2]) 
 
     titulo_stock = Label(ventana_emergente, text='Stock', font=45)
     titulo_stock.grid(row=3, column=0, padx=5, pady=5)
 
     stock_producto = Entry(ventana_emergente, font=45)
     stock_producto.grid(row=3, column=1)
-    stock_producto.insert(0, producto[3])  # Llena el campo con el stock del producto
+    stock_producto.insert(0, producto[3])  
 
     titulo_marca = Label(ventana_emergente, text='Marca', font=45)
     titulo_marca.grid(row=4, column=0)
@@ -274,7 +280,7 @@ def mostrar_formulario_edicion(producto):
     marcas_opciones = ['Seleccionar Marca', 'Arcor', 'Marolio', 'Serenisima', 'Knor', 'Natura']
 
     marca_seleccionada = StringVar()
-    marca_seleccionada.set(producto[4])  # Llena el campo con la marca del producto (suponiendo que sea una de las opciones)
+    marca_seleccionada.set(marca_seleccionada)  
 
     marca_producto = OptionMenu(ventana_emergente, marca_seleccionada, *marcas_opciones)
     marca_producto.grid(row=4, column=1)
@@ -285,12 +291,13 @@ def mostrar_formulario_edicion(producto):
     categorias_opciones = ['Seleccionar Categoria', 'Almacen', 'Bebidas', 'Lacteos', 'Golosinas', 'Aceites', 'Otros']
 
     categoria_seleccionada = StringVar()
-    categoria_seleccionada.set(producto[5])  # Llena el campo con la categoría del producto (suponiendo que sea una de las opciones)
+    categoria_seleccionada.set(categoria_seleccionada)  
 
     categoria_producto = OptionMenu(ventana_emergente, categoria_seleccionada, *categorias_opciones)
     categoria_producto.grid(row=5, column=1)
 
-    # Función para guardar los cambios en la base de datos
+    base_datos.close()
+
     def guardar_cambios():
         nombre = nombre_producto.get()
         precio = precio_producto.get()
